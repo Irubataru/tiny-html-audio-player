6ca39bbdd7e0ba92d8d57b82162dc907734f7ba8
Jonas Rylund Glesaaen <jonas@glesaaen.com>
2021-11-10 23:47:04 +0100

    Initial rewrite to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,25 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
-    entry: './src/js/tinyPlayer.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'tinyPlayer.min.js'
-    },
+    entry: './src/tinyPlayer.ts',
     module: {
         rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader']
             }
         ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'tinyPlayer.min.js'
     }
 }
